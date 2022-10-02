cd6660bb6db8e2ed3edcdcb46e29d9b4557328c6
baituole,yeshifu <1143289550@qq.com>
2022-10-02 19:07:38 +0800

    md5

---
+++ 11-MD5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_MD5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = GetMD5("123123");
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        public static string GetMD5(string str) {
+            MD5 md = MD5.Create();
+            byte[] buffer = Encoding.Default.GetBytes(str);
+            byte[] md5Buffer = md.ComputeHash(buffer);
+            string result = "";
+            for (int i = 0; i < md5Buffer.Length; i++)
+            {
+                result += md5Buffer[i].ToString("x2");
+            }
+            return result;
+            //return Encoding.Default.GetString(md5Buffer); 
+        }
+    }
+}
