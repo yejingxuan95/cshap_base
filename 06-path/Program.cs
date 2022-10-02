86c7b0b087cd7e5e27e5b4c01f5a5617c584a0c4
baituole,yeshifu <1143289550@qq.com>
2022-10-02 17:50:55 +0800

    Path类

---
+++ 06-path/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_path
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = @"D:\work\workspace\vs\c_code\01-basics-grammar\111.txt";
+            Console.WriteLine(Path.GetFileName(str));
+            Console.WriteLine(Path.GetFileNameWithoutExtension(str));
+            Console.WriteLine(Path.GetExtension(str));
+            Console.WriteLine(Path.GetDirectoryName(str));
+            Console.WriteLine(Path.GetFullPath(str));
+            Console.WriteLine(Path.Combine(@"C:\","av.txt"));
+            Console.ReadKey();
+        }
+    }
+}
