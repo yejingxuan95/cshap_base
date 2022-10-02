99fa644084fc70f7273dea42b864b3b6fad48e07
baituole,yeshifu <1143289550@qq.com>
2022-10-02 17:49:51 +0800

    dictionary

---
+++ 05-Dictionary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dict = new Dictionary<int, string>();
+            dict.Add(1,"国庆");
+            
+            dict[2] = "国庆你好";
+            foreach (KeyValuePair<int,string> item in dict)
+            {
+                Console.WriteLine($"{item.Key},{item.Value}");
+            }
+            foreach (var item in dict.Keys)
+            {
+                Console.WriteLine($"{item},{dict[item]}");
+            }
+        }
+    }
+}
