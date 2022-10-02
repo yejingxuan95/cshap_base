8b9a6c8bdb4434dfd15f381c970599430a7cb196
baituole,yeshifu <1143289550@qq.com>
2022-10-02 17:48:14 +0800

    list

---
+++ 04-List/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listStr = new List<string>();
+            listStr.Add("aaaa");
+            listStr.Add("bbb");
+            listStr.Add("aaaccca");
+
+            listStr.AddRange(new string[] { "111", "中国" });
+            for (int i = 0; i < listStr.Count; i++)
+            {
+                Console.WriteLine(listStr[i]);
+            }
+            string[] strArr = listStr.ToArray();
+
+            Console.ReadKey();
+
+        }
+    }
+}
