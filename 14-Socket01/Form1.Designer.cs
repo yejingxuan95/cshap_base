b09d1159e200216a24b8733b8c65bdf86219699c
baituole,yeshifu <1143289550@qq.com>
2022-10-04 13:10:46 +0800

    客户端服务端互发消息

--- 14-Socket01/Form1.Designer.cs
+++ 14-Socket01/Form1.Designer.cs
@@ -31,11 +31,17 @@ namespace _14_Socket01
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textsend = new System.Windows.Forms.TextBox();
+            this.butsend = new System.Windows.Forms.Button();
+            this.comboip = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(274, 12);
+            this.button1.Location = new System.Drawing.Point(60, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(181, 45);
             this.button1.TabIndex = 0;
@@ -45,21 +51,81 @@ namespace _14_Socket01
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(42, 87);
+            this.textBox1.Location = new System.Drawing.Point(41, 98);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(355, 281);
+            this.textBox1.Size = new System.Drawing.Size(268, 286);
             this.textBox1.TabIndex = 1;
+            // 
+            // textsend
+            // 
+            this.textsend.Location = new System.Drawing.Point(358, 98);
+            this.textsend.Multiline = true;
+            this.textsend.Name = "textsend";
+            this.textsend.Size = new System.Drawing.Size(255, 144);
+            this.textsend.TabIndex = 2;
+            // 
+            // butsend
+            // 
+            this.butsend.Location = new System.Drawing.Point(639, 98);
+            this.butsend.Name = "butsend";
+            this.butsend.Size = new System.Drawing.Size(97, 37);
+            this.butsend.TabIndex = 3;
+            this.butsend.Text = "发送消息";
+            this.butsend.UseVisualStyleBackColor = true;
+            this.butsend.Click += new System.EventHandler(this.butsend_Click);
+            // 
+            // comboip
+            // 
+            this.comboip.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboip.FormattingEnabled = true;
+            this.comboip.Location = new System.Drawing.Point(399, 25);
+            this.comboip.Name = "comboip";
+            this.comboip.Size = new System.Drawing.Size(252, 20);
+            this.comboip.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "控制台";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(356, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "发送消息";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(304, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "连接的客户端：";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboip);
+            this.Controls.Add(this.butsend);
+            this.Controls.Add(this.textsend);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "服务端";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -69,6 +135,12 @@ namespace _14_Socket01
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textsend;
+        private System.Windows.Forms.Button butsend;
+        private System.Windows.Forms.ComboBox comboip;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- 14-Socket01/Form1.cs
+++ 14-Socket01/Form1.cs
@@ -20,6 +20,11 @@ namespace _14_Socket01
             InitializeComponent();
         }
         
+        /// <summary>
+        /// 开启有一个Socket监听
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
             Socket socketWatch = new Socket(AddressFamily.InterNetwork, SocketType.Stream,ProtocolType.Tcp);
@@ -33,21 +38,27 @@ namespace _14_Socket01
             thread.Start(socketWatch);
             
         }
+       /// <summary>
+       /// 存储已连接的客户端ip
+       /// </summary>
+        private Dictionary<string, Socket> dictSocket = new Dictionary<string, Socket>();
 
         private void SocketListen(object socket) {
             Socket socketWatch = socket as Socket;
             while (true)
             {
                 Socket socketClient = socketWatch.Accept();
+                dictSocket[socketClient.RemoteEndPoint.ToString()]= socketClient;
+                this.comboip.Items.Add(socketClient.RemoteEndPoint.ToString());
                 ShowMsg(socketClient.RemoteEndPoint.ToString() + "连接成功");
-                Thread thread = new Thread(SocketRecea);
+                Thread thread = new Thread(SocketReceive);
                 thread.IsBackground = true;
                 thread.Start(socketClient);
 
             }
         }
 
-        private void SocketRecea(object socketClient)
+        private void SocketReceive(object socketClient)
         {
             Socket socket = socketClient as Socket;
             while (true)
@@ -67,6 +78,19 @@ namespace _14_Socket01
 
         private void ShowMsg(string msg) {
             this.textBox1.AppendText(msg + "\r\n");
+        }
+
+        private void butsend_Click(object sender, EventArgs e)
+        {
+
+            if (this.comboip.SelectedItem==null)
+            {
+                MessageBox.Show("请选择ip客户端");
+                return;
+            }
+            string key = this.comboip.SelectedItem.ToString();
+            byte[] buffer = Encoding.Default.GetBytes(this.textsend.Text);
+            dictSocket[key].Send(buffer);
         }
     }
 }
