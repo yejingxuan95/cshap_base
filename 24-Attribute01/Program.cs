0ad3abf2440cdd994ba64d05a7bc356512b1e140
baituole,yeshifu <1143289550@qq.com>
2022-10-26 19:28:46 +0800

    特性

---
+++ 24-Attribute01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_Attribute01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test("你好");
+        }
+        static void Test(string message,[CallerFilePath] string filePath ="",[CallerLineNumber] int linenumber=0,[CallerMemberName] string memberName = "") {
+            Console.WriteLine($"message:{message}");
+            Console.WriteLine($"CallerFilePath:{filePath}");
+            Console.WriteLine($"CallerLineNumber:{linenumber}");
+            Console.WriteLine($"CallerMemberName:{memberName}");
+        }
+    }
+}
