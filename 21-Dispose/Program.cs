ff83183e1e11cc551520d19f0fc3e9f65b0c2dc8
baituole,yeshifu <1143289550@qq.com>
2022-11-02 23:46:55 +0800

    dispose

---
+++ 21-Dispose/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_Dispose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (TestDispose test = new TestDispose())
+            {
+
+            }
+            Console.WriteLine("读取按键中");
+            Console.ReadKey();
+            
+            GC.Collect();
+            Console.WriteLine("读取到按键，程序结束");
+            Console.ReadKey();
+        }
+    }
+    class TestDispose:IDisposable {
+        ~TestDispose() {
+            Console.WriteLine("析构函数执行");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Dispose()");
+            //GC.SuppressFinalize(this);
+            //throw new NotImplementedException();
+        }
+    }
+}
